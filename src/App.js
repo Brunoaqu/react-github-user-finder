cc78c6cce126d50183bc06d4aeaed10a29401da0
Brunoaqu <bruno.gl.aquino@gmail.com>
2021-12-04 04:11:34 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const DisplayTable = ({ repositories }) => {
+  return (
+    <table className="table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Repositórios</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+      {repositories.map( repo => (
+        <tr>
+          <td>
+          </td>
+          <td>
+            <a href={repo.html_url} className="header" target="_blank">
+              {repo.name}
+            </a>            
+          </td>
+          <td>
+            <a className="header" target="_blank">
+              ★ {repo.stargazers_count}
+            </a>
+          </td>
+        </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const Profile = () => {
+  const [username, setUsername] = useState("");
+  const [repositories, setRepositories] = useState([]);
+
+  const onChangeHandler = e => {
+    setUsername(e.target.value);
+  };
+
+  const submitHandler = async e => {
+    e.preventDefault();
+
+    const profile = await fetch(`https://api.github.com/users/${username}`);
+    const profileJson = await profile.json();
+
+    const repositories = await fetch(profileJson.repos_url);
+    const repoJson = await repositories.json();
+    console.log(repoJson);
+
+    if (profileJson) {
+      setRepositories(repoJson);
+    }
+  };
+  return (
+    <div class="container" style={{ padding: 10 }}>
+      <div className="ui search">
+        <div className="input-group mb-3" style={{ padding: 10 }}>
+          <input className="form-control" placeholder="Digite um nome de usuário :)" type="text" value={username} onChange={onChangeHandler}/>
+          <div><button className="btn btn-outline-secondary" type="submit" onClick={submitHandler}>Pesquisar</button></div>
+        </div>
+        <DisplayTable repositories={repositories} />
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile/>
     </div>
   );
 }
-
+  
 export default App;
